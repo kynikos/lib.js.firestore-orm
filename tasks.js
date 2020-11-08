4706ef962039f6d4a67288a088ed55c9d330bb67
Dario Giovannetti <dev@dariogiovannetti.net>
2020-11-09 01:01:11 +0800

    Add todo task

--- tasks.js
+++ tasks.js
@@ -1,14 +1,39 @@
 #!/usr/bin/env node
 
 /* eslint-disable no-sync,no-await-in-loop,no-use-before-define,no-console */
+const fs = require('fs')
 const process = require('process')
 const {eslint} = require('@kynikos/tasks/subprocess')
 const {wrapCommander} = require('@kynikos/tasks/commander')
+const {reportTodo} = require('report-todo')
 
 
 function lint() {
   // See also the .eslintignore file
   return eslint([__dirname])
+}
+
+
+async function todo({labelOnly}) {
+  fs.writeFileSync(
+    './TODO.md', // report-todo-ignore-line
+    await reportTodo(
+      [
+        '.',
+        '!./.git',
+        '!./build',
+        '!./dist',
+        '!./node_modules',
+        '!./README.md',
+        '!./TODO.md', // report-todo-ignore-line
+      ],
+      {
+        labels: labelOnly ? [null] : null,
+        labelsIsBlacklist: Boolean(labelOnly),
+        reportMode: 'markdown',
+      },
+    ),
+  )
 }
 
 
@@ -18,7 +43,7 @@ const commander = wrapCommander({
   lint,
   build: false,
   runTests: false,
-  todo: false,
+  todo,
   docs: false,
   setupPkg: false,
   makePkg: false,
