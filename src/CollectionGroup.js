68a563a4d4d069f6462656c23c37aa355e5fb6ba
Dario Giovannetti <dev@dariogiovannetti.net>
2022-02-18 01:40:08 +0800

    Check pathSetups of CollectionGroup

--- src/CollectionGroup.js
+++ src/CollectionGroup.js
@@ -3,13 +3,42 @@
 // Licensed under MIT
 // https://github.com/kynikos/lib.js.firestore-orm/blob/master/LICENSE
 
-const {Query} = require('./index')
+const {CollectionSetup, DocumentSetup, Query} = require('./index')
+
+const errorMessage = "'pathSetup' must be an Array alternating the " +
+  'CollectionSetup and DocumentSetup instances that represent the setup path ' +
+  'to the expected documents; the Array must start with the root ' +
+  "CollectionSetup and end with the document's parent CollectionSetup"
 
 
 module.exports = class CollectionGroup extends Query {
   constructor({id, database, pathSetups}) {
     if (id.includes('/')) {
       throw new Error("'id' cannot be a path of segments separated by '/'")
+    }
+
+    let index = 0
+
+    while (true) {
+      const collectionSetup = pathSetups[index]
+
+      if (!collectionSetup) break
+
+      if (!(collectionSetup instanceof CollectionSetup)) {
+        throw new Error(errorMessage)
+      }
+
+      index++
+
+      const documentSetup = pathSetups[index]
+
+      if (!documentSetup) break
+
+      if (!(documentSetup instanceof DocumentSetup)) {
+        throw new Error(errorMessage)
+      }
+
+      index++
     }
 
     super({__callPostConstructor: true})
